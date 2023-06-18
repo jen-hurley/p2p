86a00ed506146e8c347d1f9668a85a6cc5dc9216
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-19 08:33:42 +1200

    add border to pixels for visual aid

--- client/components/drawpad/Row.tsx
+++ client/components/drawpad/Row.tsx
@@ -9,6 +9,7 @@ export default function Row(props: Props) {
   const { width, selectedColor } = props
 
   const pixels = []
+  // console.log(pixels + ' pixel')
 
   //push pixel component + colour into pixels array array for how ever many pixels in a row (width)
 
