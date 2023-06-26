dfa65b29e9682e0e89b58cb5a1e2c8a943cfc4fe
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-26 16:45:04 +1200

    move files and re-route client side

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import DrawPadButtons from './drawpad/draft-new-approach'
 
 function App() {
   return (
     <div>
       <h1>Welcome to Pixel 2 Pattern!</h1>
-      <DrawPadButtons />
-      {/* <Outlet /> */}
+      {/* <DrawPadButtons /> */}
+      <Outlet />
     </div>
   )
 }
---
+++ client/components/OLD/OLD-DrawPad.tsx
@@ -0,0 +1,13 @@
+// following tutorial from https://www.youtube.com/watch?time_continue=9&v=IAD68la3An8&embeds_referring_euri=https%3A%2F%2Faleksandarpopovic.com%2F&source_ve_path=Mjg2NjY&feature=emb_logo&ab_channel=AleksPopovic
+
+// npm install react-component export-image plugin
+
+import Editor from './Editor'
+
+export default function DrawPad() {
+  return (
+    <div id="App">
+      <Editor />
+    </div>
+  )
+}
