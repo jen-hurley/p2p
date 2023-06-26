e70784642c0db41810797380e216a28ca40b2d17
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-26 17:45:38 +1200

    change button text for grid setting

--- client/components/drawpad/DrawPad.tsx
+++ client/components/drawpad/DrawPad.tsx
@@ -61,7 +61,7 @@ export default function DrawPadButtons() {
     setHideDrawingPanel(!hideDrawingPanel)
 
     buttonText === 'start drawing'
-      ? setButtonText('reset')
+      ? setButtonText('change dimensions')
       : setButtonText('start drawing')
   }
 
