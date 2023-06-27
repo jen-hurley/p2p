e75a9d8c5304c25d092e2ac5795959700aa663c7
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-27 14:13:32 +1200

    set up db fn for storing pattern data

--- server/db/migrations/20230626231544_pattern.js
+++ server/db/migrations/20230626231544_pattern.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('pattern', function (table) {
+    table.increments('id').primary()
+    table.json('pattern_data')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('pattern')
+}
